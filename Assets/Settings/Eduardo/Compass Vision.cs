f655ee72bc7c8bca57f04a647e1464fc731b2bb2
Eduaryepes <eduaryepes@gmail.com>
2024-01-27 16:37:00 -0500

    Add lateral movement indicator in compass

--- Assets/Settings/Eduardo/Compass Vision.cs
+++ Assets/Settings/Eduardo/Compass Vision.cs
@@ -6,6 +6,8 @@ public class CompassVision : MonoBehaviour
 {
     [SerializeField] private GameObject arrowUpGO;
     [SerializeField] private GameObject arrowDownGO;
+    [SerializeField] private GameObject arrowLeftGO;
+    [SerializeField] private GameObject arrowRightGO;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +19,11 @@ public class CompassVision : MonoBehaviour
     void Update()
     {
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, -GameObject.Find("Main Camera").transform.rotation.eulerAngles.y));
+        SwitchArrows();
+    }
 
+    private void SwitchArrows()
+    {
         if (Input.GetKey(KeyCode.W))
         {
             arrowUpGO.SetActive(true);
@@ -34,6 +40,24 @@ public class CompassVision : MonoBehaviour
         else
         {
             arrowDownGO.SetActive(false);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            arrowLeftGO.SetActive(true);
+        }
+        else
+        {
+            arrowLeftGO.SetActive(false);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            arrowRightGO.SetActive(true);
+        }
+        else
+        {
+            arrowRightGO.SetActive(false);
         }
     }
 }
