e0166031935efc7f7c0aef291540a1f1db5c40a0
Eduaryepes <eduaryepes@gmail.com>
2024-01-28 09:37:05 -0500

    Add Fade Black UI

---
+++ Assets/Settings/Eduardo/FadeIn Black.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInBlack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void DisablePanel()
+    {
+        gameObject.SetActive(false);
+    }
+}
